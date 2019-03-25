bcacae41f60eb9b60a4a29a8b6fa5e5718b34373
Aaron Fisher <aaronmfisher86@gmail.com>
2019-03-25 15:15:49 -0400

    Added BabyShark

--- melodies.ts
+++ melodies.ts
@@ -4,3 +4,14 @@ enum Melodies {
   BabyShark = 0
 }
 
+namespace music {
+  export function getMelody(melody: Melodies): string[] {
+    switch (Melody) {
+      case Melodies.BabyShark:
+        return ['D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'G:2', 'G:2', 'F:8'];
+      default:
+        return [];
+    }
+  }
+}
+        
