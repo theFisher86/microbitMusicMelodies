53660b12ab20fdb0695eebe08bac5ba302f78e93
Aaron Fisher <aaronmfisher86@gmail.com>
2019-03-25 15:24:37 -0400

    Update melodies.ts

--- melodies.ts
+++ melodies.ts
@@ -5,8 +5,9 @@ enum Melodies {
 }
 
 namespace music {
+  //% block
   export function getMelody(melody: Melodies): string[] {
-    switch (Melody) {
+    switch (melody) {
       case Melodies.BabyShark:
         return ['D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'G:2', 'G:2', 'F:8'];
       default:
