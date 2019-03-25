327c8c84ef1aeae12697f7503cbef405af4299e6
Aaron Fisher <aaronmfisher86@gmail.com>
2019-03-25 15:26:06 -0400

    Update melodies.ts

--- melodies.ts
+++ melodies.ts
@@ -1,14 +1,14 @@
-enum Melodies {
+enum moreMelodies {
   //%block="BabyShark" blockIdentity=music.builtInMelody
   
   BabyShark = 0
 }
 
-namespace music {
+namespace moreMusic {
   //% block
-  export function getMelody(melody: Melodies): string[] {
+  export function getMelody(melody: moreMelodies): string[] {
     switch (melody) {
-      case Melodies.BabyShark:
+      case moreMelodies.BabyShark:
         return ['D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'D:4', 'E:4', 'G:2', 'G:2', 'G:2', 'G:1', 'G:2', 'G:1', 'G:2', 'G:2', 'G:2', 'F:8'];
       default:
         return [];
